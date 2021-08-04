096cffb4a4caa74b28d33e2d04ddcc38566e8221
Orestis <orestis@ep-mail.gr>
2021-08-05 00:28:41 +0300

    Produce correction result in AssignmentSubmitConsumer

--- src/kafka/consumers/assignment-submit-consumer.ts
+++ src/kafka/consumers/assignment-submit-consumer.ts
@@ -1,5 +1,6 @@
-import { Consumer, AssignmentSubmitEvent, Topics } from '@uomlms/common';
+import { Consumer, AssignmentSubmitEvent, Topics, kafka } from '@uomlms/common';
 import { Message } from 'node-rdkafka';
+import { AssignmentCorrectionProducer } from '../producers/assignment-correction-producer';
 
 
 export class AssignmentSubmitConsumer extends Consumer<AssignmentSubmitEvent> {
@@ -7,6 +8,12 @@ export class AssignmentSubmitConsumer extends Consumer<AssignmentSubmitEvent> {
 
   onMessage = (data: AssignmentSubmitEvent['data'], message: Message) => {
     console.log(data);
+    new AssignmentCorrectionProducer(kafka.producer).produce({
+      assignmentId: data.assignmentId,
+      userId: data.userId,
+      status: "sucess",
+      result: "I corrected the assignment"
+    })
   }
 
 }
